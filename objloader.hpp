88980b2c8d88aea8d934823bc73fca2b3c97c76d
Jamie Davies <jamie@jamiedavies.me>
2014-01-02 23:52:35 +0000

    Added rudimentary .obj file loader.

---
+++ objloader.hpp
@@ -0,0 +1,11 @@
+#ifndef OBJLOADER_HPP_INCLUDED
+#define OBJLOADER_HPP_INCLUDED
+
+#include <glm/glm.hpp>
+#include <vector>
+
+using namespace std;
+
+bool load_obj( const char* path, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &uvs, std::vector<glm::vec3> &normals );
+
+#endif // OBJLOADER_HPP_INCLUDED
