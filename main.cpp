1057ea1ad550455fea403449d268cfc2bf7d7b84
Jamie Davies <jamie@jamiedavies.me>
2014-01-05 18:47:21 +0000

    Drafted main key handler for the program.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,61 @@
 
 bool running = true;
 
+static void keyHandler( GLFWwindow* window, int key, int scancode, int action, int mods )
+{
+    if( action == GLFW_PRESS )
+    {
+        switch( key )
+        {
+            // Quit
+            case GLFW_KEY_ESCAPE:
+            case GLFW_KEY_Q:
+                running = false;
+                break;
+
+            // Screenshot location
+            case GLFW_KEY_P:
+                break;
+
+            // Start Tour
+            case GLFW_KEY_T:
+                break;
+
+            // End Tour
+            case GLFW_KEY_E:
+                break;
+
+            // Turn camera left
+            case GLFW_KEY_LEFT:
+                break;
+
+            // Turn camera right
+            case GLFW_KEY_RIGHT:
+                break;
+
+            // Raise camera
+            case GLFW_KEY_PAGE_UP:
+                break;
+
+            // Lower camera
+            case GLFW_KEY_PAGE_DOWN:
+                break;
+
+            // Speed up camera
+            case GLFW_KEY_UP:
+                break;
+
+            // Slow down camera
+            case GLFW_KEY_DOWN:
+                break;
+
+            // Stop camera
+            case GLFW_KEY_SPACE:
+                break;
+        }
+    }
+}
+
 bool init()
 {
 
