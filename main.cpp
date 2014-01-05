34b714709f4bf7eb1f4c87cb82ab7d3d5a52a074
Jamie Davies <jamie@jamiedavies.me>
2014-01-05 17:16:55 +0000

    Laid out basis for a (very) basic main method.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <Camera.hpp>
+#include <Model.hpp>
+
+bool running = true;
+
+bool init()
+{
+
+}
+
+bool load_models()
+{
+
+}
+
+void cleanup()
+{
+
+}
+
+int main( int argc, char* argv[] )
+{
+    init();
+
+    while( running )
+    {
+
+    }
+
+    cleanup();
+}
