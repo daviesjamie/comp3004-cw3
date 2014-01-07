1c1996d6c03bda6e15da207a6b034e328e45524b
Jamie Davies <jamie@jamiedavies.me>
2014-01-07 10:00:26 +0000

    Added Model class, to encapsulate loaded objects.

---
+++ Model.cpp
@@ -0,0 +1,68 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glimg/glimg.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+#include <memory>
+#include <stdio.h>
+
+#include "Model.hpp"
+#include "utils/objloader.hpp"
+
+Model::Model( string file )
+{
+    modelPath = file;
+}
+
+void Model::load()
+{
+    loaObj( modelPath, vertices, uvs, normals );
+
+    glGenBuffers( 3, vbo );
+    glGenVertexArrays( 1, vao );
+
+    glBindBuffer( GL_ARRAY_BUFFER, vbo[ 0 ] );
+    glBufferData( GL_ARRAY_BUFFER, vertices.size() * sizeof( glm::vec3 ), &vertices[ 0 ], GL_STATIC_DRAW );
+    glBindVertexArray( vao[ 0 ] );
+    glEnableVertexAttribArray( 0 );
+    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( glm::vec3 ), (const GLvoid*) 0 );
+
+    glBindBuffer( GL_ARRAY_BUFFER, vbo[ 1 ] );
+    glBufferData( GL_ARRAY_BUFFER, uvs.size() * sizeof( glm::vec2 ), &uvs[ 0 ], GL_STATIC_DRAW );
+    glEnableVertexAttribArray( 1 );
+    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, sizeof( glm::vec2 ), (const GLvoid*) 0 );
+
+    glBindBuffer( GL_ARRAY_BUFFER, vbo[ 2 ] );
+    glBufferData( GL_ARRAY_BUFFER, normals.size() * sizeof( glm::vec3 ), &normals[ 0 ], GL_STATIC_DRAW );
+    glEnableVertexAttribArray( 2 );
+    glVertexAttribPointer( 2, 3, GL_FLOAT, GL_FALSE, sizeof( glm::vec3 ), (const GLvoid*) 0 );
+}
+
+void Model::rotate( float amount, glm::vec3 axes )
+{
+    model = glm::rotate( model, amount, axes );
+}
+
+void Model::scale( glm::vec3 amounts )
+{
+    model = glm::scale( model, amounts );
+}
+
+void Model::translate( glm::vec3 amounts )
+{
+    model = glm::translate( model, amounts );
+}
+
+void Model::render()
+{
+    glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
+    glBindTexture( GL_TEXTURE_2D, textureID[ 0 ] );
+    glBindVertexArray( vao[ 0 ] );
+    glBindBuffer( GL_ARRAY_BUFFER, vbo[ 0 ] );
+    glDrawArrays( GL_TRIANGLES, 0, vertices.size() );
+}
+
+glm::mat4 Model::getModel()
+{
+    return model;
+}
---
+++ Model.hpp
@@ -0,0 +1,36 @@
+#ifndef MODEL_HPP_INCLUDED
+#define MODEL_HPP_INCLUDED
+
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Model
+{
+    private:
+        GLuint vao[ 1 ];
+        GLuint vbo[ 3 ];
+        string model_path;
+
+        vector<glm::vec3> vertices;
+        vector<glm::vec3> normals;
+
+        glm::mat4 model;
+
+    public:
+        Model( string file );
+
+        void load();
+
+        void rotate( float amount, glm::vec3 axes );
+        void scale( glm::vec3 amounts );
+        void translate( glm::vec3 amounts );
+
+        void render();
+
+        glm::mat4 getModel();
+};
+
+#endif // MODEL_HPP_INCLUDED
