f2ca90ef7175094d7e0ab49eecc0374fb60cf152
Jamie Davies <jamie@jamiedavies.me>
2014-01-07 08:45:00 +0000

    Removed ALL THE CODE. Starting from scratch!

--- shader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SHADER_HPP_INCLUDED
-#define SHADER_HPP_INCLUDED
-
-GLint create_shader( const char* filename, GLenum type );
-GLint link_shaders( const std::vector<GLint> &shaders );
-void printLog( GLuint object );
-
-#endif // SHADER_HPP_INCLUDED
--- utils/filetobuffer.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef FILETOBUFFER_HPP_INCLUDED
-#define FILETOBUFFER_HPP_INCLUDED
-
-char* file_to_buffer( const char* filename );
-
-#endif // FILETOBUFFER_HPP_INCLUDED
