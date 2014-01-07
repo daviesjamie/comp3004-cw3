ebaf91e4ca29e74789796ab3f99dda167cbd83cd
Jamie Davies <jamie@jamiedavies.me>
2014-01-07 10:26:00 +0000

    Fixed objloader.hpp to not include UV coords.

--- utils/objloader.hpp
+++ utils/objloader.hpp
@@ -6,6 +6,6 @@
 
 using namespace std;
 
-bool loadObj( string path, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &uvs, std::vector<glm::vec3> &normals );
+bool loadObj( string path, std::vector<glm::vec3> &vertices, std::vector<glm::vec3> &normals );
 
 #endif // OBJLOADER_HPP_INCLUDED
