6accf467037d3b4b1550fa1543c8ab497dd07329
Jamie Davies <jamie@jamiedavies.me>
2014-01-07 10:26:26 +0000

    Fixed Model.cpp to not include texture coords.

--- Model.cpp
+++ Model.cpp
@@ -11,12 +11,12 @@
 
 Model::Model( string file )
 {
-    modelPath = file;
+    model_path = file;
 }
 
 void Model::load()
 {
-    loaObj( modelPath, vertices, uvs, normals );
+    loadObj( model_path, vertices, normals );
 
     glGenBuffers( 3, vbo );
     glGenVertexArrays( 1, vao );
@@ -26,11 +26,6 @@ void Model::load()
     glBindVertexArray( vao[ 0 ] );
     glEnableVertexAttribArray( 0 );
     glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( glm::vec3 ), (const GLvoid*) 0 );
-
-    glBindBuffer( GL_ARRAY_BUFFER, vbo[ 1 ] );
-    glBufferData( GL_ARRAY_BUFFER, uvs.size() * sizeof( glm::vec2 ), &uvs[ 0 ], GL_STATIC_DRAW );
-    glEnableVertexAttribArray( 1 );
-    glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, sizeof( glm::vec2 ), (const GLvoid*) 0 );
 
     glBindBuffer( GL_ARRAY_BUFFER, vbo[ 2 ] );
     glBufferData( GL_ARRAY_BUFFER, normals.size() * sizeof( glm::vec3 ), &normals[ 0 ], GL_STATIC_DRAW );
@@ -56,7 +51,6 @@ void Model::translate( glm::vec3 amounts )
 void Model::render()
 {
     glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
-    glBindTexture( GL_TEXTURE_2D, textureID[ 0 ] );
     glBindVertexArray( vao[ 0 ] );
     glBindBuffer( GL_ARRAY_BUFFER, vbo[ 0 ] );
     glDrawArrays( GL_TRIANGLES, 0, vertices.size() );
